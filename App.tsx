d07ce3d576cf9fc4af4a3db6f05b030f8ce735c3
André Knob <dextra.aafonso@creditas.com>
2022-06-08 10:20:26 -0300

    chore: changing text do pt-br

--- App.tsx
+++ App.tsx
@@ -71,8 +71,8 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            Edite o arquivo <Text style={styles.highlight}>App.js</Text> para alterar
+            essa tela e volte parar ver suas alterações.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
